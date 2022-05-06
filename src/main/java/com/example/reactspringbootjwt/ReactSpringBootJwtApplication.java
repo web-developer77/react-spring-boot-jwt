578ab2d3a19b8f69d5fb7a3c6040ef5ee84bfb4c
DESKTOP-1NSJ62S\Eros <koa06800@gmail.com>
2022-05-06 00:32:24 -0700

    added initial spring boot auth project

---
+++ src/main/java/com/example/reactspringbootjwt/ReactSpringBootJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactspringbootjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringBootJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactSpringBootJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reactspringbootjwt/ReactSpringBootJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactspringbootjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringBootJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
